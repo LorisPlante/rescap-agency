a74d2f5cc960a03b6bd57113b180694411cb6622
Loris Planté <loris.plante@hotmail.fr>
2024-05-30 21:15:59 +0200

    first commit

---
+++ components/footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-around bg-gray-400 w-full p-3">
+      <ul>
+        <li>links</li>
+        <li>links</li>
+        <li>links</li>
+      </ul>
+      <ul>
+        <li>links</li>
+        <li>links</li>
+        <li>links</li>
+      </ul>
+      <ul>
+        <li>links</li>
+        <li>links</li>
+        <li>links</li>
+      </ul>
+    </footer>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="w-full p-3 bg-gray-400 flex justify-between items-center">
+      <div className="h-12 w-12 bg-black"></div>
+      <ul className="flex gap-4 px-4">
+        <li>links</li>
+        <li>links</li>
+        <li>links</li>
+        <li>links</li>
+      </ul>
+    </header>
+  );
+}
